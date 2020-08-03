5a2103424d02bfedb2c372f8ec8b3747f0c4ceda
admin <85103@qq.com>
2020-08-03 22:07:42 +0800

    1

--- src/arr/MyArray.java
+++ src/arr/MyArray.java
@@ -1,7 +1,7 @@
 package arr;
 
 /**
- * 数组新增
+ * 数组
  */
 public class MyArray {
 
@@ -15,7 +15,7 @@ public class MyArray {
     }
 
     /**
-     * 新增元素
+     * 新增元素-不扩容
      *
      * @param index
      * @param element
@@ -36,23 +36,91 @@ public class MyArray {
     }
 
     /**
+     * @param:
+     * @return:
+     * @Description: 在已满的数据里增加数据导致的扩容
+     * @date 2020/8/3 20:32
+     */
+    public void changeLength(int index, int element) {
+        if (index < 0 || index > size) {
+            System.out.println("越界了");
+            return;
+        }
+        //满了 扩容 <=
+        if (array.length == size) {
+            resize();
+        }
+        //size和index的关系
+        for (int i = size - 1; i >= index; i--) {
+            array[i + 1] = array[i];
+        }
+        array[index] = element;
+        size++;
+
+    }
+
+    /**
+     * @param:
+     * @return:
+     * @Description: 扩容
+     * @date 2020/8/3 21:54
+     */
+    public void resize() {
+        int[] arrayNew = new int[array.length * 2];
+        System.arraycopy(array, 0, arrayNew, 0, array.length);
+        array = arrayNew;
+    }
+
+    public void delete(int index) {
+        if (index < 0 || index > size) {
+            System.out.println("越界了");
+            return;
+        }
+        for (int i = index; i < size-1; i++) {
+            array[i] = array[i + 1];
+        }
+        size--;
+    }
+
+    /**
      * 输出
      */
     public void outPut() {
+        System.out.println("array长度=" + array.length + "  array大小=" + size);
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
     }
 
     public static void main(String[] args) {
-        MyArray myArray = new MyArray(10);
+        //新增-不扩容
+       /* MyArray myArray = new MyArray(10);
         myArray.add(0, 1);
         myArray.add(1, 2);
         myArray.add(2, 3);
         myArray.add(3, 4);
         myArray.add(4, 5);
-
         myArray.add(1, 6);
+        myArray.outPut();*/
+        //新增-扩容
+        /*MyArray myArray = new MyArray(4);
+        myArray.changeLength(0, 1);
+        myArray.changeLength(1, 2);
+        myArray.changeLength(2, 3);
+        myArray.changeLength(3, 4);
+        myArray.changeLength(4, 5);
+        myArray.changeLength(1, 6);
+        myArray.outPut();*/
+        //删除
+        MyArray myArray = new MyArray(5);
+        myArray.changeLength(0, 0);
+        myArray.changeLength(1, 1);
+        myArray.changeLength(2, 2);
+        myArray.changeLength(3, 3);
+        myArray.changeLength(4, 4);
+        myArray.changeLength(5, 5);
+        myArray.delete(1);
         myArray.outPut();
+
     }
 }
