5b7f7f2998fdec7b55b19a122b1d9ca511adf29c
admin <85103@qq.com>
2020-08-19 21:52:44 +0800

    1

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -5,20 +5,74 @@ import java.util.Arrays;
 public class BubbleSort {
 
     public static void main(String[] args) {
+        //数组特点已经包含一定的顺序
         int[] array = new int[]{3, 4, 2, 1, 5, 6, 7, 8};
-        sort(array);
+        //sort(array);
+        //sort2(array);ki
+        sort3(array);
         System.out.println(Arrays.toString(array));
     }
 
+    //升级版3 随时判断是否已经有序
+    private static void sort3(int[] array) {
+        //记录最后一次交换的位置
+        int lastExchangeIndex = 0;
+        //无序数列的边界，每次比较只需要比到这里为止
+        int sortBorder = array.length - 1;
+        for (int i = 0; i < array.length - 1; i++) {
+            System.out.println("i=" + i);
+            boolean isSorted = true;
+            for (int j = 0; j < sortBorder; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    //有元素交换，所以不是有序，标记变为false
+                    isSorted = false;
+                    //把无序数列的边界更新为最后一次交换元素的位置
+                    lastExchangeIndex = j;
+                }
+            }
+            sortBorder = lastExchangeIndex;
+            if (isSorted) {
+                break;
+            }
+        }
+    }
+
+    //原始版
     private static void sort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = 0; j < array.length - i - 1; j++) {
                 if (array[j] > array[j + 1]) {
                     int temp = array[j];
                     array[j] = array[j + 1];
-                    array[j+1] = temp;
+                    array[j + 1] = temp;
                 }
             }
         }
     }
+
+    //升级版2 一整轮完成后才能判断是否已经有序-且每一轮都是全部轮训
+    private static void sort2(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            System.out.println("i=" + i);
+            //有序标记，每一轮的初始值都是true
+            boolean isSorted = true;
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    isSorted = false;
+                }
+            }
+            if (isSorted) {
+                break;
+            }
+
+        }
+    }
+
+
 }
